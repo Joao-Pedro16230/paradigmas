359d55e4032d5a15c307fdf5c4491b2e2a0eabf1
Rafael Roesler Otto <rafaelrrotto@MacBook-Air-de-Rafael.local>
2026-08-17 20:24:04 -0300

    criação das rotas do usuário

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,26 @@
 <?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health', function() {
+    return response()->json('Minha API está online!');
+});
+
+Route::post('/users', function() {
+    $data = FacadesRequest::validate([
+        'name' => ['required', 'string', 'max:100', 'min:3'],
+        'email' => ['required', 'email'],
+        'password' => ['required', 'min:4', 'max:20']
+    ]);
+
+    User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => $data['password']
+    ]);
+
+    return response()->json('Usuário criado com sucesso!');
+});
